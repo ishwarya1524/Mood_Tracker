21c879e5fe17e0c0d8cd98655377a406f4198146
ishwarya1524 <ishwaryap1524@gmail.com>
2025-06-12 21:11:11 +0530

    height added

--- front/src/components/Mood/Mood.jsx
+++ front/src/components/Mood/Mood.jsx
@@ -18,7 +18,7 @@ const Mood = () => {
   return (
     <div className={`bg-violet-200 h-[92vh] flex flex-col justify-center items-center bg-[url('https://myfiles.123freevectors.com/images/202450-pastel-blue-simple-background.jpg')] bg-cover bg-center`}>
     <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-900 font-bold text-3xl mb-8'>Welcome to MoodTracker!</h1>
-    <form onSubmit={handlesubmit} className='bg-white h-[50vh] flex flex-col justify-center items-center  shadow-xl gap-6 rounded-lg w-100'>
+    <form onSubmit={handlesubmit} className='bg-white h-1/2 flex flex-col justify-center items-center  shadow-xl gap-6 rounded-lg w-100'>
       <input name='date' type='date' onChange={handlechange} placeholder='Enter the date' required className='border-1 shadow-sm w-90 rounded-md p-3 '/>
       <input name='mood' onChange={handlechange} placeholder='Enter the Mood' required className='border-1 p-3 w-90 rounded-md' />
       <textarea name='note' onChange={handlechange} placeholder='Enter your Thoughts..!' required className='border-1 p-3 w-90 rounded-md'/>
